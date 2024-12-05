7e025a2ae91d614ad9ab48b00ef5269083854fca
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2024-12-05 17:18:21 -0300

    chore: :wrench: installing font roboto from google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const fontRoboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  // variable: "--font-geist-sans",
 });
 
 export const metadata: Metadata = {
@@ -25,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${fontRoboto}`}>{children}</body>
     </html>
   );
 }
